d578a3757e300b6745b5dc9f2e2bab4a45de0671
Artem Derkach <artem.derkach.gs@gmail.com>
2024-06-18 22:04:40 +0300

    remove renamed dirs

--- 11_led_minimal/startup.s
+++
@@ -1,5 +0,0 @@
-// Vector table
-.section  .isr_vector, "a"
-
-	.word    0         // Top of the stack. from linker script
-	.word    _start    // ref to main function. +1 for thumb mode
--- 23_blink/startup.s
+++
@@ -1,5 +0,0 @@
-// Vector table
-.section  .isr_vector, "a"
-
-	.word    _estack   // Top of the stack. from linker script
-	.word    _start    // ref to main function. +1 for thumb mode
