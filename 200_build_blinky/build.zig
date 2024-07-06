e6865b316762f41ef9e89112e80ae7fa7034610f
Artem Derkach <artem.derkach.gs@gmail.com>
2024-07-06 15:55:06 +0300

    200_build_blinky

---
+++ 200_build_blinky/build.zig
@@ -0,0 +1,29 @@
+// openocd -f board/st_nucleo_f4.cfg -c "program zig-out/bin/main.elf verify reset exit"
+
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .thumb,
+        .os_tag = .freestanding,
+        .abi = .eabi,
+        .cpu_model = std.zig.CrossTarget.CpuModel{
+            .explicit = &std.Target.arm.cpu.cortex_m4,
+        },
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "main.elf",
+        .root_source_file = b.path("main.zig"),
+        .target = target,
+        .optimize = .ReleaseSafe,
+        .strip = true,
+    });
+
+    exe.addAssemblyFile(b.path("startup.s"));
+    exe.setLinkerScript(b.path("linker.ld"));
+    exe.bundle_compiler_rt = false;
+
+    b.install_prefix = "";
+    b.installArtifact(exe);
+}
